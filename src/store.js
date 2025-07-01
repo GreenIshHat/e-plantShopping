715321581ea9295ec34dae3332cfbb1d24760c70
name <your_email@example.com>
2025-07-01 14:38:14 +0200

    chore: prepare for gh-pages deployment

--- src/store.js
+++ src/store.js
@@ -1,8 +1,12 @@
+// src/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+
+// Create a Redux store
+const store = configureStore({
+  reducer: {
+    cart: cartReducer, // 'cart' is the slice name
+  },
 });
-export default store
+
+export default store;
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: '/e-plantShopping/', // 👈 your repo name here
   plugins: [react()],
 })
